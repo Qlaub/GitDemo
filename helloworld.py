30bb94d176e84517f85823d83fe6f48e12009a58
alex <e.pirazzi@gmail.com>
2021-11-26 21:35:32 -0500

    user 1 develop commit

--- helloworld.py
+++ helloworld.py
@@ -1,23 +1,4 @@
-from in_development import add
-
-
-def print2(word, num=1):
-    for x in range(num):
-        print(f"{word} 2")
-
-
-def pyramid(num=3):
-    for x in range(num):
-        row = ""
-        while len(row) != int(x+1):
-            row += "x"
-        print(f"row {x+1}: {row}")
-    for y in range(0, num-1):
-        row = ""
-        while len(row) != int(num - (y+1)):
-            row += "x"
-        print(f"row {num+y}: {row}")
-
+from in_development import add, print2, pyramid
 
 print2("hello world", 5)
 
--- in_development.py
+++ in_development.py
@@ -1,2 +1,20 @@
 def add(num1=0, num2=0):
     return num1 + num2
+
+
+def print2(word, num=1):
+    for x in range(num):
+        print(f"{word} 2")
+
+
+def pyramid(num=3):
+    for x in range(num):
+        row = ""
+        while len(row) != int(x+1):
+            row += "x"
+        print(f"row {x+1}: {row}")
+    for y in range(0, num-1):
+        row = ""
+        while len(row) != int(num - (y+1)):
+            row += "x"
+        print(f"row {num+y}: {row}")
