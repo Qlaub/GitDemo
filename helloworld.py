c997480305ae74106961e309471d3caf4a2b9abf
alex <e.pirazzi@gmail.com>
2021-11-26 20:40:39 -0500

    user 2 update from cmd prompt

--- helloworld.py
+++ helloworld.py
@@ -3,4 +3,19 @@ def print2(word, num=1):
         print(f"{word} 2")
 
 
+def pyramid(num=3):
+    for x in range(num):
+        row = ""
+        while len(row) != int(x+1):
+            row += "x"
+        print(f"row {x+1}: {row}")
+    for y in range(0, num-1):
+        row = ""
+        while len(row) != int(num - (y+1)):
+            row += "x"
+        print(f"row {num+y}: {row}")
+
+
 print2("hello world", 5)
+
+pyramid(5)
