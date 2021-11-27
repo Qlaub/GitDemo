8c3e0b691f133910ef3c66ca1e101f16af0d7a69
alex <e.pirazzi@gmail.com>
2021-11-26 21:40:25 -0500

    user 2 develop commit

--- helloworld.py
+++ helloworld.py
@@ -1,8 +1,11 @@
 from in_development import add, print2, pyramid
 
+# print our message a certain number of times
 print2("hello world", 5)
 
+# print a horizontal pyramid with a variable max size
 pyramid(5)
 
+# print the sum of our arguments
 x = add(2, 2)
 print(x)
